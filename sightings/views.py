4a93991359cabd2a793f23335035df55b39b031e
maximiliano vedoya ramirez <mv2779@columbia.edu>
2019-11-28 14:32:08 -0500

    Add the basic strtucture for the required views

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home_view(request, *args,**kwargs):
+    return render(request,"home.html",{})
+    
+def map_view(request, *args,**kwargs):
+    return render(request,"map.html",{})
+
+def sightings_view(request, *args,**kwargs):
+    return render(request,"sightings.html",{})
+
+def add_view(request, *args,**kwargs):
+    return render(request,"add.html",{})
