3519a412e326c16c5c988d258d7823b3c0c4a666
maximiliano vedoya ramirez <mv2779@columbia.edu>
2019-11-29 23:28:25 -0500

    Create the view /sightings/add using forms.

--- sightings/views.py
+++ sightings/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import new_sighting
+from .forms import new_sighting_form 
 
 def home_view(request, *args,**kwargs):
     return render(request,"home.html",{})
     
 def map_view(request, *args,**kwargs):
     obj=new_sighting.objects.values()
+    
     length=new_sighting.objects.count()
+    if length>100:
+        length=100
 
     location=[(obj[i]['Latitude'],obj[i]['Longitude']) for i in range(length)]
 
@@ -18,7 +22,14 @@ def map_view(request, *args,**kwargs):
     return render(request,"map.html",squirrel_location)
 
 def add_view(request, *args,**kwargs):
-    return render(request,"add.html",{})
+    form= new_sighting_form(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form=new_sighting_form()
+    context={
+        'form':form
+    }
+    return render(request,"add.html",context)
 
 def sightings_view(request):
     return render(request,"sightings.html",{})
