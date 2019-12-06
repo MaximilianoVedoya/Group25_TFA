5b0b467eb544f278cdd099ff799648fe31fa83be
maximiliano vedoya ramirez <mv2779@columbia.edu>
2019-12-06 17:29:00 -0500

    implementing bootstrap on buttons

--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -19,37 +19,40 @@ class Command(BaseCommand):
                     return(False)
                 else:
                     return(True)
-            
+           
             for squirrel in squirrels:
-                s=squirrel[5]#I use this variable to be able to parse the date form the database. 
-                p=new_sighting(
-                    Longitude=squirrel[0],
-                    Latitude=squirrel[1],
-                    Unique_Squirrel_ID =squirrel[2],
-                    Hectare=squirrel[3],
-                    Shift=squirrel[4],
-                    Date=datetime(month=int(s[0:2]),day=int(s[2:4]),year=int(s[4:8])),#Here I use the variable "s"
-                    Hectare_Squirrel_Number=squirrel[6],
-                    Age=squirrel[7],
-                    Primary_Fur_Color=squirrel[8],
-                    Location=squirrel[12],
-                    Specific_Location=squirrel[14],
-                    Running =boolean_translator(15),
-                    Chasing=boolean_translator(16),
-                    Climbing=boolean_translator(17),
-                    Eating=boolean_translator(18),
-                    Foraging=boolean_translator(19),
-                    Other_Activities=squirrel[20],
-                    Kuks=boolean_translator(21),
-                    Quaas=boolean_translator(22),
-                    Moans=boolean_translator(23),
-                    Tail_flags=boolean_translator(24),
-                    Tail_twitches=boolean_translator(25),
-                    Approaches=boolean_translator(26),
-                    Indifferent=boolean_translator(27),
-                    Runs_from=boolean_translator(28),
-                )  
-                p.save()
+                #I use this variable to be able to parse the date form the database. 
+                s=squirrel[5]
+                #verify that the ID is unique.
+                if not new_sighting.objects.filter(Unique_Squirrel_ID=squirrel[2]):
+                    p=new_sighting(
+                        Longitude=squirrel[0],
+                        Latitude=squirrel[1],
+                        Unique_Squirrel_ID= squirrel[2],
+                        Hectare=squirrel[3],
+                        Shift=squirrel[4],
+                        Date=datetime(month=int(s[0:2]),day=int(s[2:4]),year=int(s[4:8])),#Here I use the variable "s"
+                        Hectare_Squirrel_Number=squirrel[6],
+                        Age=squirrel[7],
+                        Primary_Fur_Color=squirrel[8],
+                        Location=squirrel[12],
+                        Specific_Location=squirrel[14],
+                        Running =boolean_translator(15),
+                        Chasing=boolean_translator(16),
+                        Climbing=boolean_translator(17),
+                        Eating=boolean_translator(18),
+                        Foraging=boolean_translator(19),
+                        Other_Activities=squirrel[20],
+                        Kuks=boolean_translator(21),
+                        Quaas=boolean_translator(22),
+                        Moans=boolean_translator(23),
+                        Tail_flags=boolean_translator(24),
+                        Tail_twitches=boolean_translator(25),
+                        Approaches=boolean_translator(26),
+                        Indifferent=boolean_translator(27),
+                        Runs_from=boolean_translator(28),
+                    )  
+                    p.save()
    
         
        
--- sightings/models.py
+++ sightings/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 class new_sighting (models.Model):
     
-    Latitude = models.DecimalField(max_digits=5, decimal_places=3,blank=True,null=True)
-    Longitude = models.DecimalField(max_digits=5, decimal_places=3,blank=True,null=True)
-    Unique_Squirrel_ID = models.CharField(max_length=50,blank=True,null=True)
+    Latitude = models.DecimalField(max_digits=5, decimal_places=3)
+    Longitude = models.DecimalField(max_digits=5, decimal_places=3)
+    Unique_Squirrel_ID = models.CharField(max_length=50,unique=True)
     #Not required in the problem, but I think this field is necessary to form the unique id
     Hectare=models.CharField(max_length=3,blank=True,null=True) 
     
     PM='PM'
     AM='AM'
     shift_choices=[(PM,'PM'),(AM,'AM')]
-    Shift=models.CharField(max_length=2,choices=shift_choices,blank=True,null=True)
+    Shift=models.CharField(max_length=2,choices=shift_choices)
     
-    Date= models.DateField(blank=True,null=True)
-
+    Date= models.DateField()
     #Not required in the problem, but I think this field is necessary to form the unique id
     Hectare_Squirrel_Number=models.IntegerField(blank=True,null=True)
     
@@ -32,20 +31,19 @@ class new_sighting (models.Model):
     Above_Ground='Above_Ground'
     location_choices=[(Ground_Plane,'Ground Plane'),(Above_Ground,'Above_Ground')]
     Location=models.CharField(max_length=20,choices=location_choices,blank=True,null=True) 
-    
     Specific_Location= models.CharField(max_length=50,blank=True,null=True)
-    Running = models.BooleanField(blank=True,null=True)
-    Chasing=models.BooleanField(blank=True,null=True)
-    Climbing=models.BooleanField(blank=True,null=True)
-    Eating=models.BooleanField(blank=True,null=True)
-    Foraging=models.BooleanField(blank=True,null=True)
+    Running = models.BooleanField()
+    Chasing=models.BooleanField()
+    Climbing=models.BooleanField()
+    Eating=models.BooleanField()
+    Foraging=models.BooleanField()
     Other_Activities=models.CharField(max_length=50,blank=True,null=True)
-    #maybe we need to create a relation beetween these fiels, if one is true then the other 2 must be fasle. 
-    Kuks=models.BooleanField(blank=True,null=True)
-    Quaas=models.BooleanField(blank=True,null=True)
-    Moans=models.BooleanField(blank=True,null=True)
-    Tail_flags=models.BooleanField(blank=True,null=True)
-    Tail_twitches=models.BooleanField(blank=True,null=True)
-    Approaches=models.BooleanField(blank=True,null=True)
-    Indifferent=models.BooleanField(blank=True,null=True)
-    Runs_from=models.BooleanField(blank=True,null=True)
+    
+    Kuks=models.BooleanField()
+    Quaas=models.BooleanField()
+    Moans=models.BooleanField()
+    Tail_flags=models.BooleanField()
+    Tail_twitches=models.BooleanField()
+    Approaches=models.BooleanField()
+    Indifferent=models.BooleanField()
+    Runs_from=models.BooleanField()
